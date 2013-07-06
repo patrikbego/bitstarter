8594c0f2b8118d44ab5845ecd5bec9fcee9037a0
Patrik Bego <patrik.bego@gmail.com>
2013-07-06 16:05:22 +0000

    Inserted index.html into web.js

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 var fs = require('fs');
 var buf = "";
 
-fs.readFileSync('index.html', function (err, data) {
+fs.readFile('index.html', function (err, data) {
   if (err) throw err;
 buf = data;
   console.log(data);
